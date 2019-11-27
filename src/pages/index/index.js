3d4050c945a119015c4f9e5386f1ea8fed0430b6
henryMrZhang <1065147161@qq.com>
2019-11-27 16:05:20 +0800

    'scss部分语法嵌套，继承学习'

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,5 +1,6 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
+import '../../app.scss'
 import './index.scss'
 
 export default class Index extends Component {
@@ -21,8 +22,20 @@ export default class Index extends Component {
   render () {
     return (
       <View className='index'>
-        <Text className='sasstest'>sasstest</Text>
+        {/* <Text className='sasstest'>sasstest</Text> */}
+        <View className='nestedsty'>
+          <Text className='inlinesty'>嵌套试验</Text>
+        </View>
+        <View className='top'>
+          <View className='top_txt'>复合的选择器</View>
+          <View className='top_bggrey'></View>
+        </View>
+        <View className='fontsty'>属性嵌套</View>
+        <View className='font_font'>插值试验</View>
 
+        <View className='btn_success'></View>
+        <View className='btn_error'></View>
+        <View className='btn_warning'></View>
       </View>
     )
   }
